0063f1a59cbf7f822ea5b6fb5146193a6936e4f1
umhau <umhau@alum.gcc.edu>
2017-06-14 16:49:26 -0400

    move acoustic model into lib

--- installvmc.sh
+++ installvmc.sh
@@ -17,7 +17,7 @@
 # Absolute path to this script & containing folder.  stackoverflow.com/q/242538
 script=$(readlink -f "$0"); scriptpath=$(dirname "$script") 
 
-tdir=/opt/vmc/tools; fdir=/opt/vmc/functions
+libdir=/opt/vmc/lib
 
 # CHECK FOR PREVIOUS INSTALLATION =============================================
 
@@ -35,16 +35,16 @@ fi
 sudo ls 1>/dev/null; echo -n "Installing vmc..."
 
 # create vmc directories
-sudo mkdir -p $tdir; sudo mkdir -p $fdir
+sudo mkdir -p $libdir; sudo mkdir -p $libdir
 
 # move tools
-sudo cp -r $scriptpath/tools/* $tdir/
+sudo cp -r $scriptpath/tools/* $libdir/
 
-sudo tar -xf $scriptpath/cmusphinx-en-us-ptm-5.2.tar.gz -C $tdir
-sudo mv  $tdir/cmusphinx-en-us-ptm-5.2 $tdir/en-us
+sudo tar -xf $scriptpath/cmusphinx-en-us-ptm-5.2.tar.gz -C $libdir
+sudo mv  $libdir/cmusphinx-en-us-ptm-5.2 $libdir/en-us
 
 # move functions
-sudo cp -r $scriptpath/functions/* $fdir/
+sudo cp -r $scriptpath/lib/* $libdir/
 
 # move vmc into user's path & set as executable
 sudo cp $scriptpath/vmc /usr/local/bin/vmc
@@ -57,10 +57,10 @@ sudo chmod +x /usr/local/bin/lmt
 # GET SPHINXTRAIN BINARIES ========================================================================
 
 # copy binary tools into model folder
-sudo cp /usr/local/libexec/sphinxtrain/bw $tdir
-sudo cp /usr/local/libexec/sphinxtrain/map_adapt $tdir
-sudo cp /usr/local/libexec/sphinxtrain/mk_s2sendump $tdir
-sudo cp /usr/local/libexec/sphinxtrain/mllr_solve $tdir
+sudo cp /usr/local/libexec/sphinxtrain/bw $libdir
+sudo cp /usr/local/libexec/sphinxtrain/map_adapt $libdir
+sudo cp /usr/local/libexec/sphinxtrain/mk_s2sendump $libdir
+sudo cp /usr/local/libexec/sphinxtrain/mllr_solve $libdir
 
 echo "done."
 
